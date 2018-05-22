963259ca296c5596f9b88f7630e55f57a1252ff8
makitosan <makitosansan@gmail.com>
2018-05-22 17:25:34 +0900

    Add laravel ide helper

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // ide helper
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
